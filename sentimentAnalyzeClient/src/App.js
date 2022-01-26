095ea4b256d7c436772f1cf67a6b08dd2257aacd
Neil Coulson <neil.e.j.coulson@gmail.com>
2022-01-26 00:50:38 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -43,14 +43,19 @@ class App extends React.Component {
     let mode = this.state.mode
     url = url+"/" + mode + "/sentiment?"+ mode + "="+document.getElementById("textinput").value;
 
+    console.log(url);
+
     fetch(url).then((response)=>{
         response.json().then((data)=>{
+
+        console.log('Data:' + data); 
         this.setState({sentimentOutput:data.label});
         let output = data.label;
         let color = "white"
         switch(output) {
-          case "positive": color = "black";break;
-          case "negative": color = "black";break;
+          case "positive": color = "green";break;
+          case "negative": color = "red";break;
+          case "neutral": color = "yellow";break;
           default: color = "black";
         }
         output = <div style={{color:color,fontSize:20}}>{output}</div>
